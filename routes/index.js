025b0df42dcf6b34da68deef778aeef1a7a12009
j51217123 <65345958+j51217123@users.noreply.github.com>
2024-06-03 02:47:04 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const cors = require("cors")
 const crypto = require("crypto")
 const axios = require("axios")
+const ecpay_payment = require("ecpay_aio_nodejs")
+
+const { MERCHANTID, HASHKEY, HASHIV, HOST } = process.env
 
 const corsOptions = {
     origin: "*",
@@ -74,7 +77,7 @@ const MerchantTradeDate = new Date().toLocaleDateString("zh-TW", {
     second: "2-digit",
     hour12: false,
 })
-
+let TradeNo
 //三、計算 CheckMacValue 之前
 let ParamsBeforeCMV = {
     MerchantID: MerchantID,
@@ -133,30 +136,102 @@ function CheckMacValueGen(parameters, algorithm, digest) {
     return Step6
 }
 const CheckMacValue = CheckMacValueGen(ParamsBeforeCMV, algorithm, digest)
-
-//五、將所有的參數製作成 payload
 const AllParams = { ...ParamsBeforeCMV, CheckMacValue }
 
+//五、將所有的參數製作成 payload
+const options = {
+    OperationMode: "Test", //Test or Production
+    MercProfile: {
+        MerchantID: MERCHANTID,
+        HashKey: HASHKEY,
+        HashIV: HASHIV,
+    },
+    IgnorePayment: [
+        //    "Credit",
+        //    "WebATM",
+        //    "ATM",
+        //    "CVS",
+        //    "BARCODE",
+        //    "AndroidPay"
+    ],
+    IsProjectContractor: false,
+}
+
 router.get("/test", async (req, res) => {
-    try {
-        const response = await axios.post(APIURL, new URLSearchParams(AllParams), {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        })
-        console.log("🚀 - response.data:", response.data)
-        res.send(response.data)
-    } catch (error) {
-        console.error(error)
-        res.status(500).send("Error processing payment")
+    const MerchantTradeDate = new Date().toLocaleString("zh-TW", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+        timeZone: "UTC",
+    })
+    TradeNo = "test" + new Date().getTime()
+    let base_param = {
+        MerchantTradeNo: TradeNo, //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+        MerchantTradeDate,
+        TotalAmount: "100",
+        TradeDesc: "測試交易描述",
+        ItemName: "測試商品等",
+        ReturnURL: `${HOST}/return`,
+        ClientBackURL: `${HOST}/clientReturn`,
     }
+    TradeNo = "test" + new Date().getTime()
+
+    // try {
+    //     const response = await axios.post(APIURL, new URLSearchParams(AllParams), {
+    //         headers: {
+    //             "Content-Type": "application/x-www-form-urlencoded",
+    //         },
+    //     })
+    //     console.log("🚀 - response.data:", response.data)
+    //     res.send(response.data)
+    // } catch (error) {
+    //     console.error(error)
+    //     res.status(500).send("Error processing payment")
+    // }
+    const create = new ecpay_payment(options)
+
+    // 注意：在此事直接提供 html + js 直接觸發的範例，直接從前端觸發付款行為
+    const htm = create.payment_client.aio_check_out_all(base_param)
+    console.log(htm)
+
+    res.render("index", {
+        title: "Express",
+        htm,
+    })
 })
 
-router.post("/payment/callback", (req, res) => {
-    // 處理綠界金流的回調通知
-    console.log(req.body)
-    // 根據回調通知的內容更新訂單狀態等操作
-    res.send("OK")
+// router.post("/payment/callback", (req, res) => {
+//     // 處理綠界金流的回調通知
+//     console.log(req.body)
+//     // 根據回調通知的內容更新訂單狀態等操作
+//     res.send("OK")
+// })
+
+// 後端接收綠界回傳的資料
+router.post("/return", async (req, res) => {
+    console.log("req.body:", req.body)
+
+    const { CheckMacValue } = req.body
+    const data = { ...req.body }
+    delete data.CheckMacValue // 此段不驗證
+
+    const create = new ecpay_payment(options)
+    const checkValue = create.payment_client.helper.gen_chk_mac_value(data)
+
+    console.log("確認交易正確性：", CheckMacValue === checkValue, CheckMacValue, checkValue)
+
+    // 交易成功後，需要回傳 1|OK 給綠界
+    res.send("1|OK")
+})
+
+// 用戶交易完成後的轉址
+router.get("/clientReturn", (req, res) => {
+    console.log("clientReturn:", req.body, req.query)
+    res.render("return", { query: req.query })
 })
 
 module.exports = router
